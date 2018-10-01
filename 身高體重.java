1af5bd5e8c1fea97ac816c25aec46a551dde59f6
HanksCheng <43198381+HanksCheng@users.noreply.github.com>
2018-10-01 17:29:31 +0800

    Update 身高體重.java

--- 身高體重.java
+++ 身高體重.java
@@ -4,8 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scn=new Scanner(System.in);
-        int h=scn.nextInt();
-        int w=scn.nextInt();
+        double h=scn.nextInt();
+        double w=scn.nextInt();
         System.out.println(h/2.54);
         System.out.println(w/0.454);
     }
