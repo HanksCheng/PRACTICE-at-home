45ead36de9344f1d4f666f8108c8f5e685118984
HanksCheng <43198381+HanksCheng@users.noreply.github.com>
2018-10-07 02:13:03 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scn= new Scanner(System.in);
+        float c=scn.nextFloat();
+        System.out.println(c*9/5+32);
+    }
+}
